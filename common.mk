9e9eac4f8257d8eb02db15acc232385784c1dd73
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2013-09-16 15:35:45 +0530

    fix mount_all for All devices

--- common.mk
+++ common.mk
@@ -15,6 +15,9 @@
 # Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+##Apollo
+PRODUCT_PACKAGES += \
+        Apollo 
 ## Audio
 PRODUCT_PACKAGES += \
     audio_policy.msm7x27 \
@@ -146,14 +149,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ## SAMSUNG_BOOTLOADER is the product model changed into appropriate string parsed by init.
 ## Example: -GT-I5500 becomes gt-i5500board, -GT-S5830 becomes gt-s5830board, and so on.
 SAMSUNG_BOOTLOADER := $(shell echo $(PRODUCT_VERSION_DEVICE_SPECIFIC)board | tr '[A-Z]' '[a-z]' | cut -c 2-)
-
-PRODUCT_PACKAGES += \
-    charging.rle \
-    fstab.$(SAMSUNG_BOOTLOADER) \
-    init.$(SAMSUNG_BOOTLOADER).bluez.rc \
-    init.$(SAMSUNG_BOOTLOADER).parts.rc \
-    init.$(SAMSUNG_BOOTLOADER).usb.rc \
-    ueventd.$(SAMSUNG_BOOTLOADER).rc
+PRODUCT_COPY_FILES += \
+    device/samsung/msm7x27-common/ramdisk/fstab.msm7x27:root/fstab.msm7x27 \
+    device/samsung/msm7x27-common/ramdisk/fstab.msm7x27:root/fstab.$(SAMSUNG_BOOTLOADER) \
+    device/samsung/msm7x27-common/ramdisk/init.msm7x27.rc:root/init.$(SAMSUNG_BOOTLOADER).rc \
+    device/samsung/msm7x27-common/ramdisk/init.msm7x27.bluez.rc:root/init.$(SAMSUNG_BOOTLOADER).bluez.rc \
+    device/samsung/msm7x27-common/ramdisk/init.msm7x27.parts.rc:root/init.$(SAMSUNG_BOOTLOADER).parts.rc \
+    device/samsung/msm7x27-common/ramdisk/init.msm7x27.usb.rc:root/init.$(SAMSUNG_BOOTLOADER).usb.rc \
+    device/samsung/msm7x27-common/ramdisk/ueventd.msm7x27.rc:root/ueventd.$(SAMSUNG_BOOTLOADER).rc
 
 # Inherit qcom/msm7x27
 $(call inherit-product, device/qcom/msm7x27/msm7x27.mk)
